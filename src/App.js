7a065cdef50543831a9848eabcc37cfc7b7e488d
FluroAsh <workashleythompson@gmail.com>
2022-05-17 01:05:56 +1000

    adds spotify user authorisation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React from 'react';
 
 function App() {
+  // capital-named constants are only used as aliases for “hard-coded” values
+  const CLIENT_ID = '406c8a0b7edd439dae2018d8f93a2473';
+  const REDIRECT_URI = 'http://localhost:3000';
+  const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+  const RESPONSE_TYPE = 'token';
+
   return (
     <div className="app">
-      <h1>Hello World!</h1>
+      <header>
+        <h1>Spotify React</h1>
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+        >
+          Login to Spotify
+        </a>
+      </header>
     </div>
   );
 }
